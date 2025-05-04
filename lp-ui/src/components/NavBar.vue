f1d76702e85a0769a6cc5a582075bab72fa34472
MrHarryW <wheeler_harry@icloud.com>
2025-05-04 21:30:08 +0100

    Added more to home

--- lp-ui/src/components/NavBar.vue
+++ lp-ui/src/components/NavBar.vue
@@ -6,6 +6,10 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
       </div>
+      <img
+      style="height: 50px;"
+      src="../../public/logotp.png"
+    >
     <a class="btn btn-ghost text-xl" style="color: #5865f2;">LaunchPad Framework</a>
   </div>
   <div class="navbar-center hidden lg:flex">
--- lp-ui/src/views/HomeView.vue
+++ lp-ui/src/views/HomeView.vue
@@ -2,21 +2,49 @@
 </script>
 
 <template>
-  <div class="hero bg-base-200 min-h-150">
+  <div class="hero bg-base-200 min-h-150" style="background-image: linear-gradient(0deg, #191e24, #d1d5ff);> --light:background-image-linear-gradient(0deg, #d1d5ff, #fff);"> "
   <div class="hero-content flex-col lg:flex-row-reverse">
     <div class="mockup-code w-full min-h-100">
   <pre data-prefix="$"><code>npm i launchpad.py</code></pre>
   <pre data-prefix=">" class="text-warning"><code>installing...</code></pre>
   <pre data-prefix=">" class="text-success"><code>Done!</code></pre>
 </div>
-    <div class="max-w-screen" style="float: left;">
-      <h1 class="text-5xl font-bold">LaunchPad Framework</h1>
-      <p class="py-6" style="font-size: 40px;">
+    <div class="max-w-screen" style="float: left; padding-right: 60px;">
+      <h1 class="text-2xl font-bold">LaunchPad Framework</h1>
+      <p class="py-3" style="font-size: 40px;">
         Your Launchpad for Powerful Discord Bots. 
       </p>
-      <button class="btn btn-primary">Get Started</button>
+      <button class="btn btn-primary" style="margin: 5px;">Quick Start</button>
+      <button class="btn btn-primary" style="margin: 5px;">Sponsor</button>
     </div>
   </div>
 </div>
+
+<div>
+    <h1 class="text-4xl font-bold bg-base-200" style="padding-left: 40px;">Mission Control Members</h1>
+  </div>
+
+
+<div class="hero bg-base-200 min-h-60">
+<a href=""><div class="card w-60 h-30 bg-base-100 card-xs shadow-sm">
+  <div class="card-body" style="justify-content: center; text-align: center;">
+    <h2 class="text-xl font-bold" style="color: gray;">Sponsor LaunchPad</h2>
+  </div>
+</div></a>
+</div>
+
+<div>
+    <h1 class="text-4xl font-bold bg-base-200" style="padding-left: 40px;">Docking Bay Members</h1>
+  </div>
+
+
+<div class="hero bg-base-200 min-h-60">
+<a href=""><div class="card w-60 h-30 bg-base-100 card-xs shadow-sm">
+  <div class="card-body" style="justify-content: center; text-align: center;">
+    <h2 class="text-xl font-bold" style="color: gray;">Sponsor LaunchPad</h2>
+  </div>
+</div></a>
+</div>
+
 </template>
 
